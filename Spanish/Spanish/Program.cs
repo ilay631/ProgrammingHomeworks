ad9467a188a61ac7e3dec0bf9ee8917f5a119d6b
Prusskikh Ilya <prusskih.ilay@gmail.com>
2023-09-15 00:38:42 +0300

    Added spanish task (Not finished)

---
+++ Spanish/Spanish/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spanish
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number [0; 10^9]");
+            if (!InputNumber(out int num)) { return; }
+
+            Console.WriteLine(TranslateToSpanish(num));
+
+            Console.ReadLine();
+        }
+
+        static bool InputNumber(out int value)
+        {
+            if (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("[ERROR]: Invalid input");
+                Console.ReadLine();
+                return false;
+            }
+            if (!(0 <= value && value <= Math.Pow(10, 9)))
+            {
+                Console.WriteLine("[ERROR]: Invalid input");
+                Console.ReadLine();
+                return false;
+            }
+            return true;
+        }
+
+        static string TranslateToSpanish(int num)
+        {
+            return "";
+        }
+    }
+}
