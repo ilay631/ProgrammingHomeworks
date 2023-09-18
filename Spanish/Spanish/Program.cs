fd5521e183d4c7b1af4495d8c02df97ac550bc57
Prusskikh Ilya <prusskih.ilay@gmail.com>
2023-09-19 02:35:04 +0300

    Implemented functions GetSpanish2Digit and GetSpanishDigit. Add unit tests for this functions with humanizer.

--- Spanish/Spanish/Program.cs
+++ Spanish/Spanish/Program.cs
@@ -3,24 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
+
 
 namespace Spanish
 {
-    internal class Program
+    public class Program
     {
+        public enum Gender
+        {
+            Masculine,
+            Feminine,
+            Neuter
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Enter number [0; 10^9]");
-            if (!InputNumber(out int num)) { return; }
-
-            Console.WriteLine(TranslateToSpanish(num));
+            if (!InputNumber(out uint num)) { return; }
+            Console.WriteLine("Enter gender (male, female, neutral)");
+            switch (Console.ReadLine())
+            {
+                case "male":
+                    Console.WriteLine(TranslateToSpanish(num, Gender.Masculine));
+                    break;
+                case "female":
+                    Console.WriteLine(TranslateToSpanish(num, Gender.Feminine));
+                    break;
+                case "neutral":
+                    Console.WriteLine(TranslateToSpanish(num, Gender.Neuter));
+                    break;
+                default:
+                    Console.WriteLine("[ERROR] Invalid gender");
+                    break;
+            }
 
             Console.ReadLine();
         }
 
-        static bool InputNumber(out int value)
+        static bool InputNumber(out uint value)
         {
-            if (!int.TryParse(Console.ReadLine(), out value))
+            if (!uint.TryParse(Console.ReadLine(), out value))
             {
                 Console.WriteLine("[ERROR]: Invalid input");
                 Console.ReadLine();
@@ -35,8 +58,47 @@ namespace Spanish
             return true;
         }
 
-        static string TranslateToSpanish(int num)
+        public static string GetSpanishDigit(uint digit, Gender gen)
         {
+            string[] spanishDigits = { "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" };
+            if (digit == 1)
+                switch(gen)
+                {
+                    case Gender.Masculine:
+                        return "uno";
+                    case Gender.Feminine:
+                        return "una";
+                    case Gender.Neuter:
+                        return "uno";
+                }
+            return spanishDigits[digit];
+        }
+
+        public static string GetSpanish2Digit(uint num, Gender gen)
+        {
+            // From 10 to 29
+            string[] spanish2Digits = { "diez", "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve", "veinte", "veintiuno", "veintidós", "veintitrés", "veinticuatro", "veinticinco", "veintiséis", "veintisiete", "veintiocho", "veintinueve" };
+            string[] spanishDozens = { "diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
+            if (num == 21)
+                switch (gen)
+                {
+                    case Gender.Masculine:
+                        return "veintiuno";
+                    case Gender.Feminine:
+                        return "veintiuna";
+                    case Gender.Neuter:
+                        return "veintiuno";
+                }
+            if (num < 30)
+                return spanish2Digits[num - 10];
+            if (num % 10 == 0)
+                return spanishDozens[num / 10 - 1];
+            return spanishDozens[num / 10 - 1] + " y " + GetSpanishDigit(num % 10, gen);
+        }
+
+        static string TranslateToSpanish(uint num, Gender gen)
+        {
+
             return "";
         }
     }
---
+++ Spanish/SpanishTests/ProgramTests.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spanish;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+using System.Globalization;
+
+namespace Spanish.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GetSpanishDigitMasculineTest()
+        {
+            // Arrange
+            string[] expected = new string[10];
+            for (int i = 0; i < expected.Length; i++)
+                expected[i] = i.ToWords(GrammaticalGender.Masculine, new CultureInfo("es-SP"));
+
+            // Act
+            string[] res = new string[10];
+            for (uint i = 0; i < res.Length; i++)
+                res[i] = Program.GetSpanishDigit(i, Program.Gender.Masculine);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod()]
+        public void GetSpanishDigitFeminineTest()
+        {
+            // Arrange
+            string[] expected = new string[10];
+            for (int i = 0; i < expected.Length; i++)
+                expected[i] = i.ToWords(GrammaticalGender.Feminine, new CultureInfo("es-SP"));
+
+            // Act
+            string[] res = new string[10];
+            for (uint i = 0; i < res.Length; i++)
+                res[i] = Program.GetSpanishDigit(i, Program.Gender.Feminine);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod()]
+        public void GetSpanishDigitNeuterTest()
+        {
+            // Arrange
+            string[] expected = new string[10];
+            for (int i = 0; i < expected.Length; i++)
+                expected[i] = i.ToWords(GrammaticalGender.Neuter, new CultureInfo("es-SP"));
+
+            // Act
+            string[] res = new string[10];
+            for (uint i = 0; i < res.Length; i++)
+                res[i] = Program.GetSpanishDigit(i, Program.Gender.Neuter);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod()]
+        public void GetSpanish2DigitMasculineTest()
+        {
+            // Arrange
+            string[] expected = new string[90];
+            for (int i = 0; i < expected.Length; i++)
+                expected[i] = (i + 10).ToWords(GrammaticalGender.Masculine, new CultureInfo("es-SP"));
+
+            // Act
+            string[] res = new string[90];
+            for (uint i = 0; i < res.Length; i++)
+                res[i] = Program.GetSpanish2Digit(i + 10, Program.Gender.Masculine);
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine("{0} - {1} - {2}", i + 10, expected[i], res[i]);
+            }
+
+            // Assert
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod()]
+        public void GetSpanish2DigitNeuterTest()
+        {
+            // Arrange
+            string[] expected = new string[90];
+            for (int i = 0; i < expected.Length; i++)
+                expected[i] = (i + 10).ToWords(GrammaticalGender.Neuter, new CultureInfo("es-SP"));
+
+            // Act
+            string[] res = new string[90];
+            for (uint i = 0; i < res.Length; i++)
+                res[i] = Program.GetSpanish2Digit(i + 10, Program.Gender.Neuter);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, res);
+        }
+
+        [TestMethod()]
+        public void GetSpanish2DigitFeminineTest()
+        {
+            // Arrange
+            string[] expected = new string[90];
+            for (int i = 0; i < expected.Length; i++)
+                expected[i] = (i + 10).ToWords(GrammaticalGender.Feminine, new CultureInfo("es-SP"));
+
+            // Act
+            string[] res = new string[90];
+            for (uint i = 0; i < res.Length; i++)
+                res[i] = Program.GetSpanish2Digit(i + 10, Program.Gender.Feminine);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, res);
+        }
+    }
+}
